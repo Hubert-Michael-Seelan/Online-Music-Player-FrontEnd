7dba94b7aa4045d9aac9eb30a1ec77ad93e9e890
Hubert-Michael-Seelan <hubertmichaelseelan@gmail.com>
2023-09-13 17:05:06 +0530

    Newly added

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import { Home, Login } from "./components";
+import React, { useEffect, useState } from "react";
+import { app } from "./config/firebase.config";
+import { getAuth } from "firebase/auth";
+import { AnimatePresence } from "framer-motion";
+import { validateUser } from "./api";
+
+function App() {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged((userCred) => {
+      if (userCred) {
+        userCred.getIdToken().then((token) => {
+          // console.log(token);
+          validateUser(token).then((data)=>{
+            console.log(data);
+          })
+        });
+      } else {
+        setAuth(false);
+        window.localStorage.setItem("auth", "false");
+        navigate("/login");
+      }
+    });
+  }, []);
+  return (
+    <AnimatePresence mode="wait">
+      <div className="h-auto min-w-[680px] bg-primary flex justify-center items-center">
+        <Routes>
+          <Route path="/login" element={<Login setAuth={setAuth} />} />
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </div>
+    </AnimatePresence>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '.'
+
+const Home = () => {
+  return (
+    <div className="w-full h-auto flex flex-col items-center justify-center bg-primary">
+      <Header /> 
+    </div>
+  )
+}
+
+export default Home
