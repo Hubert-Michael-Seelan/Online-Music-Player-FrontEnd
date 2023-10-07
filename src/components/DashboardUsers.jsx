e9a2acba4f5500ecc205113a4d03bd402cae7083
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2023-10-07 13:18:22 +0530

    added new file dashboard users

--- src/components/DashboardUsers.jsx
+++ src/components/DashboardUsers.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useStateValue } from "../context/StateProvider";
 
 const DashboardUsers = () => {
+  const [{ allUsers }, dispatch] = useStateValue();
   return (
-    <div>DashboardUsers</div>
-  )
-}
+    <div className="w-full p-4 flex items-center justify-center flex-col">
+      <div className="relative w-full py-12 min-h-[400px] overflow-x-scroll scrollbar-thin scrollbar-track-slate-300 scrollbar-thumb-slate-400 my-4 flex flex-col items-center justify-start p-4 border border-gray-300 rounded-md gap-3">
+        <div className="absolute top-4 left-4">
+          <p className="text-sm font-semibold text-textColor">
+            Count :{" "}
+            <span className="text-xl font-bold">{allUsers?.length} </span>
+          </p>
+        </div>
+        <div className="w-full min-w-[750px] flex items-center justify-between">
+          {/* prettier-ignore */}
+          <p className="text-sm text-textColor font-semibold w-275 min-w-[160px] text-center">Image</p>
+          {/* prettier-ignore */}
+          <p className="text-sm text-textColor font-semibold w-275 min-w-[160px] text-center">Name</p>
+          {/* prettier-ignore */}
+          <p className="text-sm text-textColor font-semibold w-275 min-w-[160px] text-center">Email</p>
+          {/* prettier-ignore */}
+          <p className="text-sm text-textColor font-semibold w-275 min-w-[160px] text-center">Verified</p>
+          {/* prettier-ignore */}
+          <p className="text-sm text-textColor font-semibold w-275 min-w-[160px] text-center">Created</p>
+          {/* prettier-ignore */}
+          <p className="text-sm text-textColor font-semibold w-275 min-w-[160px] text-center">Role</p>{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default DashboardUsers
+export default DashboardUsers;
