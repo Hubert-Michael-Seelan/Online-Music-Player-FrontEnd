41ca36621efa4706a24230431fac33814494eb5c
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2023-10-07 19:56:41 +0530

    newly added

--- src/api/index.js
+++ src/api/index.js
@@ -51,3 +51,23 @@ export const getAllAlbums = async () => {
   }
 };
 
+export const changingUserRole = async (userId, role) => {
+  try {
+    const res = axios.put(`${baseURL}api/users/updateRole/${userId}`, {
+      data: { role: role },
+    });
+    return res;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const removeUser = async (userId) => {
+  try {
+    const res = axios.delete(`${baseURL}api/users/delete/${userId}`);
+    return res;
+  } catch (error) {
+    return null;
+  }
+};
+
